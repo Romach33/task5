1b7b366a6673d9f1cdb4ada1c381b3b4b7ecf5f9
Romach33 <d.romach33@mail.ru>
2020-11-16 21:21:01 +0300

    task5

--- src/com/company/number2/Main.java
+++ src/com/company/number2/Main.java
@@ -1,13 +1,13 @@
 package com.company.number2;
 
-        import java.util.Scanner;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         int h = readInt();
         char ch = 'a';
-        printTriangle(h,ch);
+        printTriangle(h, ch);
     }
 
     private static int readInt() {
@@ -17,17 +17,12 @@ public class Main {
     }
 
     public static void printTriangle(int h, char ch) {
-        for (int i = 1; i <= h; i++)
-        {
-            for(int count = 1; count<=i; count++)
-            {
+        for (int i = 1; i <= h; i++) {
+            for (int count = 1; count <= i; count++) {
                 System.out.print(ch);
-                if (ch == 'a')
-                {
+                if (ch == 'a') {
                     ch++;
-                }
-                else
-                {
+                } else {
                     ch--;
                 }
             }
